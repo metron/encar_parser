9f92af86c244b2e99bec8cf89415ffc8ac1eb0fb
Akim Negrey <metron19@yandex.ru>
2026-04-04 14:21:33 +0300

    move another disk

---
+++ frontend/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/api/data')
+      .then(res => res.json())
+      .then(setData);
+  }, []);
+
+  if (!data) return <div>Загрузка...</div>;
+
+  return (
+    <div>
+      <h1>{data.message}</h1>
+      <ul>
+        {data.users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
